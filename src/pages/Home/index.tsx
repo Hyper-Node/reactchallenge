e3e9b157702f2254f7eb8bf7522c121d8a09f889
Stegmujo <johannesst@gmx.de>
2023-12-17 16:08:12 +0100

    Fix "known" to bold

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,8 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import { useTranslation } from "react-i18next";
+import { Trans } from 'react-i18next'
+
 
 const Home = () => {
   const { t } = useTranslation("app");
@@ -51,22 +53,25 @@ const Home = () => {
           {t("home.welcome")}
         </Typography>
         <Typography variant="subtitle1" textAlign="center">
-          {t("home.intro")}{" "}
+          <Trans
+              i18nKey="home.intro"
+              components={{ b: <strong /> }}
+          />
         </Typography>
         <Typography variant="body2" textAlign="center" color="textSecondary">
           {t("home.sidenote")}
         </Typography>
         <List>
           {issues.map((issue) => (
-            <ListItem>
-              <Typography variant="h5" sx={{ p: 2 }}>
-                {issue.icon}
-              </Typography>
-              <ListItemText
-                primary={issue.title}
-                secondary={issue.description}
-              />
-            </ListItem>
+              <ListItem>
+                <Typography variant="h5" sx={{p: 2}}>
+                  {issue.icon}
+                </Typography>
+                <ListItemText
+                    primary={issue.title}
+                    secondary={issue.description}
+                />
+              </ListItem>
           ))}
         </List>
       </Container>
